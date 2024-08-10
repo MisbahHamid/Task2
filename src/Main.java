e6127a61ba1ab9bdfe058917619e8edb30f4f171
Misbah <misbahhamid74@gmail.com>
2024-08-10 12:52:38 +0500

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,35 @@
-//Take a date input from string and add 30 days on that date and print new date in dd-mmyyyy this formate?
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input
+//string is valid.
+//• An input string is valid if:
+//        • Open brackets must be closed by the same type of brackets.
+//• Open brackets must be closed in the correct order.
+//• Every close bracket has a corresponding open bracket of the same type.
+//        Example 1:
+//        Input: s = "()"
+//        Output: true
+//        Example 2:
+//        Input: s = "()[]{}"
+//        Output: true
+//        Example 3:
+//        Input: s = "(]"
+//        Output: false
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a date");
-        String str=sc.nextLine();
-        LocalDate date = LocalDate.parse(str);
-        date = date.plusDays(30);
-        System.out.println(date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String str = sc.nextLine();
+        if(Pattern.compile("\\(\\)|\\{\\}|\\[\\]|\\(\\{\\[\\]}\\)").matcher(str).matches())
+            System.out.println("valid");
+        else
+            System.out.println("invalid");
 
     }
 }
+
